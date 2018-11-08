bf6ae913362c244c6ba622851b8b31edf98ff187
iskybow <kjionmd@gmail.com>
2018-11-08 14:29:15 +0300

    new page: contacts, partners and brand

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -8,19 +8,53 @@ var Shared = function Shared() {
 
 window.onload = function (e) {
   //dropdown
-  $('.jsDropOpen').mouseenter(function () {
-    $(this).parent('li').children('.drop-menu').addClass('drop-active');
+  if (window.innerWidth > 993) {
+    $('.jsDropOpen').mouseenter(function () {
+      $(this).parent('li').children('.drop-menu').addClass('drop-active');
+    });
+    $('.jsDropClose').mouseleave(function () {
+      $(this).parent('li').children('.drop-menu').removeClass('drop-active');
+    });
+  } else {
+    $('.jsDropOpen').click(function () {
+      $(this).parent('.jsParentDropMenu').children('.drop-menu').toggleClass('drop-active');
+    });
+  }
+
+  $('.jsMobMenu').click(function () {
+    $('.jsMenu').toggleClass('menu-active');
+    $('.overlay').fadeToggle();
+    $('.jsMobMenu').toggleClass('burger-active');
+    $('body').toggleClass('body-overflow');
   });
-  $('.jsDropClose').mouseleave(function () {
-    $(this).parent('li').children('.drop-menu').removeClass('drop-active');
+  $('.overlay').click(function () {
+    $('.jsMenu').removeClass('menu-active');
+    $('.overlay').fadeToggle();
+    $('.jsMobMenu').removeClass('burger-active');
+    $('body').removeClass('body-overflow');
   });
 
   //search
-  var searchBtn = document.querySelector('.jsSearch');
-  var searchBlock = document.querySelector('.jsSearchBlock');
-  searchBtn.onclick = function () {
-    searchBlock.classList.toggle('search-active');
-  };
+  $('.jsSearchOpen').click(function () {
+    $('.jsSearchOpen').addClass('search-open-noactive');
+    $('.search-block input').addClass('search-active');
+    setTimeout(function () {
+      $('.search-block input').addClass('searchFlag');
+    }, 2000);
+  });
+
+  $(document).click(function (e) {
+    // событие клика по веб-документу
+    var input = $(".search-block input"); // тут указываем ID элемента
+    var inputBlock = $('.jsSearchBlock');
+    if (!input.is(e.target) // если клик был не по нашему блоку
+    && inputBlock.has(e.target).length === 0 && input.hasClass('searchFlag')) {
+      // и не по его дочерним элементам
+      input.removeClass('search-active'); // скрываем его
+      $('.jsSearchOpen').removeClass('search-open-noactive');
+      input.removeClass('searchFlag');
+    }
+  });
 };
 
 jQuery(document).ready(function ($) {

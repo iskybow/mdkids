6b94a1b11caa4cc3d9ae1126c0d1ae786f37a112
kennerus <kennerus@gmail.com>
2018-11-28 11:42:45 +0300

    error modal

---
+++ src/components/modal-error/modal-error.js
@@ -0,0 +1,16 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//   const errorModal = document.querySelector('.js-modalError');
+//   const errorModalClose = document.querySelectorAll('.js-modalErrorClose');
+//   const modalBtn = document.querySelector('.modal button[type="submit"]');
+//
+//   modalBtn.addEventListener('click', e => {
+//     e.preventDefault();
+//     errorModal.classList.add('modal-active');
+//   });
+//
+//   for (let i = 0; i < errorModalClose.length; i++) {
+//     errorModalClose[i].addEventListener('click', () => {
+//       errorModal.classList.remove('modal-active');
+//     })
+//   }
+// });

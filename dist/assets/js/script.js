83135904b66b1354e6c5318c7960a97121e31302
iskybow <kjionmd@gmail.com>
2018-11-28 13:46:01 +0300

    error modal

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -114,6 +114,26 @@ $(document).on('click', '.modal-close', function () {
 // });
 
 
+$(document).ready(function () {
+  if ($('.jsPhoneMask').length > 0) {
+    $('.jsPhoneMask').inputmask({ alias: "phoneru" });
+  }
+
+  $('.jsBirthday').select2();
+
+  $('.jsRightGender').click(function () {
+    $('.jsGenderSwitch').addClass('gender-switch-right');
+    $('.jsLeftGender').removeClass('gender-active-label');
+    $('.jsRightGender').addClass('gender-active-label');
+  });
+
+  $('.jsLeftGender').click(function () {
+    $('.jsGenderSwitch').removeClass('gender-switch-right');
+    $('.jsLeftGender').addClass('gender-active-label');
+    $('.jsRightGender').removeClass('gender-active-label');
+  });
+});
+
 $(function () {
   if (window.innerWidth < 1200) {
     $('.cabinet-nav').addClass('touch-menu-la');
@@ -180,26 +200,6 @@ $(document).ready(function () {
     var orderBlock = this.closest('.orders-item');
     orderBlock.remove();
     totalPrice();
-  });
-});
-
-$(document).ready(function () {
-  if ($('.jsPhoneMask').length > 0) {
-    $('.jsPhoneMask').inputmask({ alias: "phoneru" });
-  }
-
-  $('.jsBirthday').select2();
-
-  $('.jsRightGender').click(function () {
-    $('.jsGenderSwitch').addClass('gender-switch-right');
-    $('.jsLeftGender').removeClass('gender-active-label');
-    $('.jsRightGender').addClass('gender-active-label');
-  });
-
-  $('.jsLeftGender').click(function () {
-    $('.jsGenderSwitch').removeClass('gender-switch-right');
-    $('.jsLeftGender').addClass('gender-active-label');
-    $('.jsRightGender').removeClass('gender-active-label');
   });
 });
 
